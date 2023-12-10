fd09414bf4b87d07a4f1540981a224485824fbb2
DouglasFeliphe <douglasfeliphe@hotmail.com>
2023-12-10 00:41:22 -0300

    feat: add global styles

---
+++ src/GlobalStyles.ts
@@ -0,0 +1,43 @@
+/* fonts */
+export const FontFamily = {
+  uppercaseSmall: 'Poppins-SemiBold',
+  bodySecondary: 'Poppins-Medium',
+  sFProText: 'SF Pro Text',
+  bodyRegular14: 'Poppins-Regular',
+};
+/* font sizes */
+export const FontSize = {
+  uppercaseSmall_size: 10,
+  size_4xs: 9,
+  size_mini: 15,
+  bodyRegular14_size: 14,
+  titleH3_size: 16,
+  titleH1_size: 24,
+  bodySecondary_size: 12,
+  titleH2_size: 18,
+};
+/* Colors */
+export const Color = {
+  backgroundWhite: '#fff',
+  textDark60: '#686777',
+  textDark100: '#04021d',
+  colorBlueviolet: '#554af0',
+  backgroundExtraWhite: '#f8f8fa',
+  accentStrongBlue10: '#eeedfe',
+  textDark80: '#36354a',
+  statusRed: '#ff0000',
+};
+/* Paddings */
+export const Padding = {
+  p_5xs: 8,
+  p_xs: 12,
+  p_base: 16,
+};
+/* border radiuses */
+export const Border = {
+  br_11xs_3: 1,
+  br_10xs_7: 3,
+  br_xl: 20,
+  br_xs: 12,
+  br_base: 16,
+};
