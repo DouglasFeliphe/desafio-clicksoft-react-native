1e59ee3c11e70e35733b19943624458a78786bda
DouglasFeliphe <douglasfeliphe@hotmail.com>
2023-12-11 02:24:47 -0300

    chore: edit absolute imports config

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,31 @@
-module.exports = function(api) {
+module.exports = function (api) {
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver',
+        {
+          root: '.',
+          extensions: [
+            '.js',
+            '.jsx',
+            '.ts',
+            '.tsx',
+            '.android.js',
+            '.android.tsx',
+            '.ios.js',
+            '.ios.tsx',
+          ],
+          alias: {
+            '@components': './src/components',
+            '@shared-components': './src/shared/components',
+            '@screens': './src/screens',
+            '@routes': './src/routes',
+            '@mock': './src/mock',
+          },
+        },
+      ],
+    ],
   };
 };
